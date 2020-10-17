81ddeb33aaf0fc3d68d7ec6c45c5b68d57e2dcbe
NuruzzamanKhan <shuvopcc2018@gmail.com>
2020-10-17 23:31:30 +0600

    double-vs-triple-equal clear for interview

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,11 @@
+// 1 = true
+// 0 = false
+// === check type and value
+
+const first = 2;
+const second = '2';
+if(first === second){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
