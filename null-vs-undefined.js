dcb1946ea968102512240380fa4ce02f0d8f60c1
NuruzzamanKhan <shuvopcc2018@gmail.com>
2020-10-17 23:23:00 +0600

    null vs undefined clear for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+let pakhi;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const premik = {name:"smrt dude", phone:02154}
+console.log(premik.gf);
+
+let fun = undefined;
+
+
+console.log(premik.gf);
